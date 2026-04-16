02cfad104459b56196aa18d6907db23a6f07ca14
Dari Pinheiro <contatodariph27@gmail.com>
2026-04-15 21:18:12 -0300

    Meu primeiro projeto via SSH

---
+++ app.js
@@ -0,0 +1,7 @@
+const tecnologias = ["Node.js", "Express", "PostgreSQL", "SSH Authentication"];
+
+console.log("🚀 Iniciando Trilha Back-end...");
+
+tecnologias.forEach((tech) => {
+  console.log(`✅ Estudar: ${tech}`);
+});
