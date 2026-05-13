ca1512bb9cb3a79b508f1d642229adddedf48007
Dari Pinheiro <contatodariph27@gmail.com>
2026-05-13 15:23:49 -0300

    primeiro commit

--- app.js
+++
@@ -1,7 +0,0 @@
-const tecnologias = ["Node.js", "Express", "PostgreSQL", "SSH Authentication"];
-
-console.log("🚀 Iniciando Trilha Back-end...");
-
-tecnologias.forEach((tech) => {
-  console.log(`✅ Estudar: ${tech}`);
-});
